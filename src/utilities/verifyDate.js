399958b315eaeb222e10aae505cd9290665ff493
elPedron <pedrohenriquesilva@aluno.senai.br>
2024-12-05 12:05:06 -0300

    feat: verifyDate function

---
+++ src/utilities/verifyDate.js
@@ -0,0 +1,17 @@
+const verifyDate = (dateString) => {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+    if(!dateRegex.test(dateString)) return false; // valida formato da data yyyy-mm-dd
+
+    const date = new Date(dateString);
+
+    if(isNaN(date.getTime())) return false; // Verifica se a data é inválida (Date.getTime() retorna NaN para datas inválidas
+
+    const [year, month, day] = dateString.split('-').map(Number);
+    
+    return(
+        date.getUTCFullYear() === year &&
+        date.getUTCMonth() === month - 1 &&
+        date.getUTCDate() === day
+    );
+}
