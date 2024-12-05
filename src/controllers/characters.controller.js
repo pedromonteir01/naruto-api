ccbb12b8b1df07a83c889848157e5443379fd8a9
elPedron <pedrohenriquesilva@aluno.senai.br>
2024-12-05 10:33:23 -0300

    fix: scripts sql

--- src/controllers/characters.controller.js
+++ src/controllers/characters.controller.js
@@ -1,16 +1,16 @@
 const pool = require('../config/database.config');
 
-export const getAllCharacters = async(req, res) => {
+const getAllCharacters = async (req, res) => {
     try {
         const response = await pool.query('SELECT * FROM characters');
-        if(response.rowCount == 0) {
-            return res.status(404).send({ message: 'nenhum personagem encontrado' });
+        if (response.rowCount === 0) {
+            return res.status(404).send({ message: 'Nenhum personagem encontrado' });
         } else {
             return res.status(200).send(response.rows);
         }
-    } catch(e) {
-        return res.status(500).send({ error: 'erro de servidor' });
+    } catch (e) {
+        return res.status(500).send({ error: 'Erro de servidor' });
     }
-}
+};
 
-module.exports = { getAllCharacters }
+module.exports = { getAllCharacters };
--- src/database/scripts.sql
+++ src/database/scripts.sql
@@ -3,33 +3,34 @@ CREATE DATABASE narutodata;
 \c narutodata;
 
 CREATE TABLE characters (
-    name VARCHAR(256),
-    description TEXT,
-    image TEXT,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    description TEXT NOT NULL,
+    image TEXT NOT NULL,
     metadata JSONB,
-    gender CHAR(1),
-    birthdate DATETIME
+    gender CHAR(1) NOT NULL,
+    birthdate DATE
 );
 
 CREATE TABLE affiliations (
-    name VARCHAR(256),
-    image TEXT
+    name VARCHAR(256) PRIMARY KEY,
+    image TEXT NOT NULL
 );
 
 CREATE TABLE chakras (
-    name VARCHAR(15),
-    image TEXT,
-    description TEXT
+    name VARCHAR(15) PRIMARY KEY,
+    image TEXT NOT NULL,
+    description TEXT NOT NULL
 );
 
 CREATE TABLE kekkei_genkai (
-    name VARCHAR(100),
-    image TEXT,
-    description TEXT
+    name VARCHAR(100) PRIMARY KEY,
+    image TEXT NOT NULL,
+    description TEXT NOT NULL
 );
 
 CREATE TABLE kekkei_touta (
-    name VARCHAR(15),
-    image TEXT,
-    description TEXT
+    name VARCHAR(15) PRIMARY KEY,
+    image TEXT NOT NULL,
+    description TEXT NOT NULL
 );
